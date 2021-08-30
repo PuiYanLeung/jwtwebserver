0f7aa4b08d323e3970384d548ba2e7504d7ead6d
PuiYanLeung <trewina@gmail.com>
2021-08-30 18:25:01 +0100

    auth session

--- auth.js
+++ auth.js
@@ -5,12 +5,14 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const User = require("./models/user");
 
-const register = (name, password, done) =>{
+const register = async(name, password, done) =>{
     const saltRounds = 10;
+
     try{
         if(!name){
             throw new Error("A name wa not provided");
         }
+
         const salt = await bcrypt.genSalt(saltRounds);
         const hash = await bcrypt.hash(password, salt);
         const user = await User.build({name, passwordHash: hash});
@@ -21,16 +23,45 @@ const register = (name, password, done) =>{
         }catch(error){
             done(null, {});
         }
-
     }catch(error){
         done(error);
     }
 };
-const verify = () =>{};
-const login = () =>{};
+
+const verify = async(token, done) =>{
+    try{
+        done(null, token.user);
+    }catch(error){
+        done(error);
+    }
+};
+
+const login = async(username, password, done) =>{
+    try{
+        const user = await User.findOne({where:{name: username}});
+
+        if (!user){
+            return done(null, false, {msg:"Incorrect Username"});
+        }
+
+        const match = await bcrypt.compare(password, user.passwordHash);
+        return match ? done (null, user) : done(null, false, {msg:"Incorrect Password"});
+    }catch(error){
+        done(error);
+    }
+};
+
+const verifyStrategy = new JWTStrategy({
+    secretOrKey: process.env.SECRET_KEY,
+    jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token")
+}, verify);
 
 const registerStrategy = new LocalStrategy({usernameField: 'name', passwordField: 'password'}, register);
 
+const loginStrategy = new LocalStrategy(login);
+
 modeule.exports = {
-    registerStrategy
+    registerStrategy,
+    verifyStrategy,
+    loginStrategy
 }
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,38 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
 const router = express.Router();
 
-router.post("/register", (req,res) =>{
-    console.log(req.body);
-    res.status(200).json({msg: "register route"});
-});
+const profile = async(req, res, next) =>{
+    res.status(200).json({msg: "Profile", user: req.user, token: req.query.secret_token});
+};
 
-router.post("/login", (req, res)=>{
-    res.status(200).json({msg: "login route"});
-});
+const register = async(req, res, next) =>{
+    req.user.name ? res.status(200).json({msg: "registered successfully", user: req.user}): res.status(401).json({msg:"User already exists"});
+};
 
-router.get("/profile", (req, res)=>{
-    res.status(200).json({msg: "profile"});
-});
+const login = async(req, res, next) => {
+    const session = {session: false};
+
+    passport.authenticate("login", async(err, user, info)=>{
+        try{
+            if (err){
+                res.status(500).json({msg:"Internal Server Error"});
+            }else if (!user){
+                res.status(401).json({msg: "User not found"});
+            }else{
+                req.login(user, session, async(error)=> error ? next(error): res.status(200).json({user, token: jwt.sign({user: {id: user.id, name: user.name}})}));
+            }
+        }catch (error){
+            return next(error);
+        }
+    })(req, res, next);
+};
+
+router.post("/register", register);
+router.get("/profile", profile);
+router.post("/login", login);
+
 
 module.exports = router;
